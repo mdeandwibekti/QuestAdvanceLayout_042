a113fc22302c0dda35b8374ed5bea25620e03bbd
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-24 16:13:41 +0700

    menambahkan warna pada card

--- app/src/main/java/com/example/questadvancelayout_042/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_042/UIPertama.kt
@@ -51,7 +51,9 @@ fun ActivitasPertama(modifier: Modifier){
                 .height(height = 160.dp)
                 .fillMaxWidth(fraction = 1f)
                 .padding(all = 12.dp),
-
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
         ){
 
 }
