7caf7dd180026b2af1364b104d5515be9f52df08
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-24 16:02:08 +0700

    membuat funActivitypertama

--- app/src/main/java/com/example/questadvancelayout_042/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_042/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_042Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
