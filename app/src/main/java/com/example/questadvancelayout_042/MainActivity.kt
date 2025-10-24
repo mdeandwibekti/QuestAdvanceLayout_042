3d1e2391ffb098ab61befba97f6abaea833c615a
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-24 19:45:22 +0700

    membuat fun Praktikum3

--- app/src/main/java/com/example/questadvancelayout_042/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_042/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_042Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ActivitasPertama(
+                    Praktikum3(
 
                         modifier = Modifier.padding(innerPadding)
                     )
